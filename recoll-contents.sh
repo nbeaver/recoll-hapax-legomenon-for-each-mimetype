f51c602537352862a95a15de47a833c388148b00
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-07-29 11:24:28 -0500

    Don't need this anymore.

--- recoll-contents.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-if ! test -f "$*"
-then
-    printf 'Usage: %s /path/to/file\n' "$0"
-fi
-filename="$*"
-# Use command substitution instead of `cat`.
-recoll -t "$(< $filename)"
