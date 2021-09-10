dc0b779d75c800a2395387e73441c1ab64a4ea6e
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2021-09-09 23:51:11 -0400

    Add simple python script for when 'find' command is unavailable.

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ TIMESTAMP:=$(shell date +%F_%s)
 regenerate :
 	./generate_files.py $(TEMPLATES)/ $(TOP_DIR)/
 	touch $(MANUAL_DIR)/*
-	find $(TOP_DIR)/ -print | recollindex -e -i
-	find $(MANUAL_DIR)/ -print | recollindex -e -i
+	./print_filepaths.py $(TOP_DIR)/ | recollindex -e -i
+	./print_filepaths.py $(MANUAL_DIR)/ | recollindex -e -i
 	./get-file-mimetypes.sh $(TOP_DIR)/* $(MANUAL_DIR)/* > file-mimetypes.txt
 	./get-mimetypes.sh $(TOP_DIR)/* $(MANUAL_DIR)/* | sort | uniq > mimetypes.txt
 	./search_for_unique_strings.py $(TOP_DIR)/hapax_list.txt $(MANUAL_DIR)/manual_mappings.txt 2>&1 | less
@@ -35,7 +35,7 @@ file-mimetypes.txt :
 
 .PHONY: index
 index :
-	find $(TOP_DIR)/ -print | recollindex -e -i
+	./print_filepaths.py $(TOP_DIR)/ -print | recollindex -e -i
 
 .PHONY: search
 search :
---
+++ print_filepaths.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+import argparse
+import os
+import logging
+import sys
+
+def yield_filepaths(topdir):
+    for dirpath, dirnames, filenames in os.walk(topdir, topdown=True):
+        for filename in filenames:
+            fullpath = os.path.join(dirpath, filename)
+            if os.path.isfile(fullpath):
+                yield fullpath
+            else:
+                logging.debug("Skipping path '{}'".format(filepath))
+                continue
+
+def readable_directory(path):
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(
+            'not an existing directory: {}'.format(path))
+    if not os.access(path, os.R_OK):
+        raise argparse.ArgumentTypeError(
+            'not a readable directory: {}'.format(path))
+    return path
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='List files under a directory, recursively')
+    parser.add_argument(
+        'topdir', type=readable_directory, help='Top directory')
+    # https://stackoverflow.com/questions/14097061/easier-way-to-enable-verbose-logging
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='More verbose logging',
+        dest="loglevel",
+        default=logging.WARNING,
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        '-d',
+        '--debug',
+        help='Enable debugging logs',
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+    )
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
+
+    for path in yield_filepaths(args.topdir):
+        sys.stdout.write(path + '\n')
+
+if __name__ == '__main__':
+    main()
