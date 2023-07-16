c1f1b58ca228ec10eac4b2cf3a2e6666f0df0cad
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2023-07-16 13:26:00 -0400

    Add argument to print realpaths in case of symlinks in path.

--- print_filepaths.py
+++ print_filepaths.py
@@ -5,12 +5,16 @@ import os
 import logging
 import sys
 
-def yield_filepaths(topdir):
+def yield_filepaths(topdir, real_paths=False):
     for dirpath, dirnames, filenames in os.walk(topdir, topdown=True):
         for filename in filenames:
             fullpath = os.path.join(dirpath, filename)
             if os.path.isfile(fullpath):
-                yield fullpath
+                if real_paths:
+                    real_path = os.path.realpath(fullpath)
+                    yield real_path
+                else:
+                    yield fullpath
             else:
                 logging.debug("Skipping path '{}'".format(filepath))
                 continue
@@ -48,10 +52,15 @@ def main():
         dest="loglevel",
         const=logging.DEBUG,
     )
+    parser.add_argument(
+        '--real-paths',
+        help='Print real paths',
+        action="store_true",
+    )
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
 
-    for path in yield_filepaths(args.topdir):
+    for path in yield_filepaths(args.topdir, real_paths=args.real_paths):
         sys.stdout.write(path + '\n')
 
 if __name__ == '__main__':
